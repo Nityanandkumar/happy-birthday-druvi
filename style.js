8348fad48306f4c2d7b7418d8eae2b6c681d0626
Nityanandkumar <62872277+Nityanandkumar@users.noreply.github.com>
2020-08-17 16:41:45 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+alert("Touch the cake ");
+alert(" I repeat dont forget to touch the cake");
+
+window.addEventListener("click", function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
